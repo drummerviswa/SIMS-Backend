0f3c02755085653e770fdd19b59d17e09beac1b2
Viswanathan P <drummerviswa@gmail.com>
2025-02-14 07:28:27 +0530

    Feat: Added DB, Img upload, etc 🚀

---
+++ src/app.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+import multer from "multer";
+
+//Importing middlewares
+import { cloudinary } from "./middlewares/cloudinary.js";
+
+//Express app creation
+const app = express();
+
+//Middlewares
+app.use(express.urlencoded({ extended: false }));
+dotenv.config();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+//Cloudinary setup
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Define a POST route for uploading an image
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  console.log("Uploaded file:", req.file);
+  cloudinary.uploader.upload(req.file.path, (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Failed to upload image to Cloudinary", details: err });
+    }
+
+    console.log("Cloudinary upload result:", result);
+    res.json({ imageUrl: result.secure_url });
+  });
+});
+
+//App listener
+const port = process.env.PORT || 8800;
+app.listen(port, () => {
+  console.log(`Api started 🤍 at ${process.env.BASE_URL + ":" + port}`);
+});
